b989b1e4304aa60bd76a0c228d2c200044b7a4b6
Yixin LIU <eisennoah@outlook.com>
2016-03-24 22:10:36 +0800

    Update Journey.java
    edit space

--- src/Journey.java
+++ src/Journey.java
@@ -8,13 +8,13 @@
 public class Journey implements Iterable{
     private String journeyId;
     private ArrayList<Stop> timetable;
-	public Iterator<Stop> iterator() {
+    public Iterator<Stop> iterator() {
         return new StopIterator();
     }
 	// iterator of stops
-	private class StopIterator implements Iterator<Stop> {
-	    public boolean hasNext() { }
+    private class StopIterator implements Iterator<Stop> {
+	public boolean hasNext() { }
         public void remove() { }
         public Stop next() { }
-	}
+    }
 }
